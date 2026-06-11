3793c2eaa88373b3c865efe45c99ba847134cb08
luanna <luannamuller1981@gmail.com>
2026-06-11 12:12:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,129 @@
+/* mensagens */
 
 function abrirMensagem() {
-  alert('Eu te amo meu amor !!!!!!!!!!');
+alert("Eu te amo meu amor !!!!!!!!!!");
 }
 
-const mensagens = [
-  '',
-  '',
-  '',
-  '',
-  ''
+
+let mensagens = [
+  "Eu te amo. Soube disso no minuto em que te conheci. ♡",
+  "Eu amo como você faz eu me sentir como se tudo fosse possível, ou como se tudo valesse a pena. ♡",
+  "Em cada pedaço de mim, sempre haverá um pedaço de você. ♡",
+  "Obrigada por me amar. ♡"
 ];
 
-function trocarMensagem() {
-  const texto = document.getElementById('textoMensagem');
+let i = 0;
 
-  const random = Math.floor(Math.random() * mensagens.length);
+function mensagemSurpresa(){
 
-  texto.innerText = mensagens[random];
+if (i < mensagens.length){
+document.getElementById("mensagemFofa").innerHTML = mensagens[i];
+  i++;
+ }
 }
-// Desde o dia que te conheci, sabia que você era a pessoa certa para mim.
-// Desde o dia que fizemos nosso primeiro contato visual, não pude deixar de me apaixonar pelo que vi em seus olhos. 
-// Tanta pureza, que enxerga beleza até no que não é notado. 
-// seu jeito unico de falar, agir, pensar, fez com que me apaixonasse completamente por voc. 
-// me apaixonei completamente pela sua alma//
+
+
+/* trocar de paginas */
+
+const limparPagina = document.querySelectorAll("section")
+function inicio(){
+limparPagina.forEach(function(paginas){
+  paginas.classList.remove("ativa")
+})
+
+document.getElementById("inicio").classList.add("ativa")
+
+}
+function sobre(){
+limparPagina.forEach(function(paginas){
+  paginas.classList.remove("ativa")
+})
+
+document.getElementById("sobre").classList.add("ativa")
+
+}
+function nos(){
+limparPagina.forEach(function(paginas){
+  paginas.classList.remove("ativa")
+})
+
+document.getElementById("nos").classList.add("ativa")
+
+}
+function galeria(){
+limparPagina.forEach(function(paginas){
+  paginas.classList.remove("ativa")
+})
+
+document.getElementById("galeria").classList.add("ativa")
+
+}
+function mensagem(){
+limparPagina.forEach(function(paginas){
+  paginas.classList.remove("ativa")
+})
+
+document.getElementById("mensagem").classList.add("ativa")
+}
+
+
+/* musica*/
+
+const musica = document.getElementById("music");
+musica.volume = 0.15;
+let tocando = false;
+
+function tocarMusica(){
+const botao = document.getElementById("musicBtn");
+
+if(!tocando){
+  musica.play();
+  botao.innerHTML = "pausar!!";
+  tocando = true;
+}else{
+  musica.pause();
+  botao.innerHTML = "tocar!!";
+  tocando = false;
+ }
+}
+
+
+/* estrelas de fundo */
+
+const starsContainer = document.getElementById("estrelas")
+
+for(let i = 0; i < 250; i++){
+
+const estrela = document.createElement("div");
+estrela.classList.add("estrela");
+
+estrela.style.left = Math.random() * 100 + "vw";
+
+estrela.style.top = Math.random() * 100 + "vh";
+
+estrela.style.animationDelay = Math.random() * 5 + "s";
+
+starsContainer.appendChild(estrela);
+}
+
+
+/* brilho do mouse */
+
+document.addEventListener("mousemove", (mouse) => {for (let i = 0; i < 3; i++) {
+
+const brilho = document.createElement("div");
+
+brilho.className = "sparkle";
+
+brilho.style.left =
+mouse.clientX + (Math.random() * 20 - 10) + "px";
+
+brilho.style.top =
+mouse.clientY + (Math.random() * 20 - 10) + "px";
+
+document.body.appendChild(brilho);
+
+setTimeout(() => {brilho.remove();}, 1000);
+}
+
+});
